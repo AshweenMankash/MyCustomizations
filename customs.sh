67ff4a65bc2b84a7f429ac5310b899d789fbc5c5
Ashween <ashweenmankash@gmail.com>
2023-09-11 23:39:46 +0530

    Added git configs

--- customs.sh
+++ customs.sh
@@ -1,1 +1,3 @@
+git config --global user.name Ashween Mankash
+git config --global user.email ashweenmankash@gmail.com
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
